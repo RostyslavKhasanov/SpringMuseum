b6f5a97ccc2e53c56eb660e1715619d47e81d696
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-07-25 13:26:54 +0300

    change annotations in dao

--- src/main/java/museum/dao/AuthorDao.java
+++ src/main/java/museum/dao/AuthorDao.java
@@ -1,9 +1,13 @@
 package museum.dao;
 
 import museum.entity.Author;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public class AuthorDao extends ElementDaoImpl<Author> {
   public AuthorDao() {
     super(Author.class);
--- src/main/java/museum/dao/ExhibitDao.java
+++ src/main/java/museum/dao/ExhibitDao.java
@@ -1,9 +1,13 @@
 package museum.dao;
 
 import museum.entity.Exhibit;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public class ExhibitDao extends ElementDaoImpl<Exhibit> {
   public ExhibitDao() {
     super(Exhibit.class);
