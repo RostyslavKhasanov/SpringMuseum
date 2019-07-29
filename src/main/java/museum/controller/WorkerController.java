917d9e7b39a92a833460c67796c0e31221b02a21
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-07-29 20:02:13 +0300

    add javadoc

--- src/main/java/museum/controller/WorkerController.java
+++ src/main/java/museum/controller/WorkerController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 /**
- * Core servlet controller class for 'worker' page.
+ * Controller class for 'worker' page.
  *
  * @author Rostyslav Khasanov
  * @version 1.0
