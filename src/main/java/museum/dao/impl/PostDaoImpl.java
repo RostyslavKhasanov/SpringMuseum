5b9557ba37412ea31d828b0eef804a021265cd17
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-07-29 20:13:33 +0300

    add javadoc

--- src/main/java/museum/dao/impl/PostDaoImpl.java
+++ src/main/java/museum/dao/impl/PostDaoImpl.java
@@ -5,7 +5,12 @@ import museum.dao.PostDao;
 import museum.entity.Post;
 import org.springframework.stereotype.Repository;
 
-
+/**
+ * DAO implementation for Post entity.
+ *
+ * @author Rostyslav Khasanov
+ * @version 1.0
+ */
 @Repository
 public class PostDaoImpl extends ElementDaoImpl<Post> implements PostDao {
     public PostDaoImpl() {
