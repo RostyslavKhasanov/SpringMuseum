ed62cd8fe12bc762cc49f8ce6047e51f72a8ce8a
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-07-30 09:23:14 +0300

    add javadoc

--- src/main/java/museum/service/WorkerService.java
+++ src/main/java/museum/service/WorkerService.java
@@ -17,23 +17,75 @@ import java.util.List;
  */
 public interface WorkerService {
 
+  /**
+   * Save worker.
+   *
+   * @param workerAddRequestDto request worker dto.
+   */
   void save(WorkerAddRequestDto workerAddRequestDto);
 
+  /**
+   * Get all workers.
+   *
+   * @return List of workerFirstSecondNameDtoResponse;
+   */
   List<WorkerFirstSecondNameDtoResponse> findAll();
 
+  /**
+   * Get worker by id.
+   *
+   * @param id worker id.
+   * @return worker;
+   */
   Worker findById(Long id);
 
+  /**
+   * Get worker id by name.
+   *
+   * @param name worker name.
+   * @return id of worker;
+   */
   Long findWorkerIdByName(String name);
 
+  /**
+   * Get all free guides.
+   *
+   * @return List of WorkerDtoResponse;
+   */
   List<WorkerDtoResponse> findAllFreeGuide();
 
+  /**
+   * Get all guides.
+   *
+   * @return List of WorkerDtoResponse;
+   */
   List<WorkerDtoResponse> findAllGuide();
 
+  /**
+   * Get guides statistic.
+   *
+   * @return List of WorkerStatDtoResponse;
+   */
   List<WorkerStatDtoResponse> findGuidesStat();
 
+  /**
+   * Delete worker by id.
+   *
+   * @param id worker id.
+   */
   void deleteById(Long id);
 
+  /**
+   * Get worker by id..
+   *
+   * @return Worker;
+   */
   Worker getOneById(Long id);
 
+  /**
+   * Update worker info.
+   *
+   * @param workerUpdateRequestDto request worker dto.
+   */
   void update(WorkerUpdateRequestDto workerUpdateRequestDto);
 }
