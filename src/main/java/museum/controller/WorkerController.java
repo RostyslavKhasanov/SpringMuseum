fe976c8308c9a42f97e947d256af597903480561
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-05 18:28:45 +0300

    added  validation

--- src/main/java/museum/controller/WorkerController.java
+++ src/main/java/museum/controller/WorkerController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
 /**
